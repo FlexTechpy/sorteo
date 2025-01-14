6c02d5240a362db67f460e15ad3eba5444578541
FlexTechpy <108410944+FlexTechpy@users.noreply.github.com>
2025-01-14 00:23:06 -0300

    probando

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3932738906"
+    serviceWorkerVersion: "1392662035"
   }
 });
